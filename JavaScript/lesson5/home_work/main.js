576b89d570a2d8a50bb82bb3482f26ec74e7af53
Beloshapkin_AMG <dmytro.beloshapkin@gmail.com>
2022-05-03 17:20:38 +0300

    work 06.05

--- JavaScript/lesson5/home_work/main.js
+++ JavaScript/lesson5/home_work/main.js
@@ -1,5 +1,19 @@
 // Все стірлочними!!!!!!!!!
 let arrey = [100, 10, 20, 5, 30, 40, 50];
+let arrey2 = [10, 50];
+let users = [
+    {id: 1, name: 'Dima', age: 36, status: true},
+    {id: 2, name: 'Marianna', age: 37, status: true},
+    {id: 3, name: 'Vitaliy', age: 8, status: false},
+    {id: 4, name: 'Jura', age: 4, status: false},
+    {id: 5, name: 'Tetiana', age: 66, status: false},
+    {id: 6, name: 'Olya', age: 58, status: false},
+    {id: 7, name: 'Vasul', age: 32, status: true},
+    {id: 8, name: 'Khristina', age: 35, status: true},
+    {id: 9, name: 'Oleksandr', age: 5, status: false},
+    {id: 10, name: 'Anna', age: 2, status: false}
+];
+
 // - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
 // let average = (arr) => {
 //     let start = 0;
@@ -53,23 +67,120 @@ let arrey = [100, 10, 20, 5, 30, 40, 50];
 // console.log(arrRandom(10));
 
 // - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
+// let reverArrey = (arr) => {
+//     let newArr = [];
+//     for (let i = arr.length-1; i >= 0; i--) {
+//         const newArrElement = arr[i];
+//         newArr.push(newArrElement)
+//     }
+//     return newArr
+// }
+// console.log(reverArrey(arrey));
 
 
+// Переробити на стрілочні функції з попереднього дз
 
-
-
-
-//     Переробити на стрілочні функції з попереднього дз
 // - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+// let triangleSquare = (a, b) => a * b
+// console.log(triangleSquare(5, 10));
+
 // - створити функцію яка обчислює та повертає площу кола з радіусом r
+// let radiusСircle = (r) => 3.14 * Math.pow(r, 2);
+// console.log(radiusСircle(5))
+
 // - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+// let cylinderАrea = (h, r) =>2 * 3.14 * r * h
+// console.log(cylinderАrea(5, 2));
+
 // - створити функцію яка приймає масив та виводить кожен його елемент
+// let arreyAcept = (arr) => {
+//     for (const user of arr) {
+//     console.log(user)
+//     }
+// }
+// arreyAcept(arrey)
+
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+// let paragraphMaker = (txt) => document.write(`<p>${txt}</p>`)
+// paragraphMaker(`куку йопта`)
+
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
-// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
-// - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
-// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
-// - створити функцію яка повертає найменьше число з масиву
-// - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
+// let ulMaker = (txt1, txt2, txt3) => {
+//     document.write(`<ul>`)
+//     document.write(`<li>${txt1}`)
+//     document.write(`<li>${txt2}`)
+//     document.write(`<li>${txt3}`)
+//     document.write(`</ul>`)
+// }
+// ulMaker(`куку`, `привіт`, `працює`)
+
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий.
+// Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+// let ulMakerPro = (txt, length) => {
 //
+//     document.write(`<ul>`)
+//
+//     for (let i = 0; i < length; i++) {
+//         const txtElement = txt[i];
+//         document.write(`<li>${txt}`)
+//     }
+//
+//     document.write(`</ul>`)
+// }
+// ulMakerPro(`тест`, 70)
+
+// - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+// let arrMakerLow = (arr) => {
+//     document.write(`<ul>`)
+//     for (const user of arr) {
+//         document.write(`<li>${user}`)
+//     }
+//     document.write(`</ul>`)
+// }
+// arrMakerLow(arrey)
+
+// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+// let arrMakerPro = (arr) => {
+//     for (const arrElement of arr) {
+//         document.write(`<div>`)
+//         document.write(`<li>${arrElement.id}</li>`)
+//         document.write(`<li>${arrElement.name}</li>`)
+//         document.write(`<li>${arrElement.age}</li>`)
+//         document.write(`</div>`)
+//         document.write(`<hr>`)
+//     }
+// }
+// arrMakerPro(users)
+
+// - створити функцію яка повертає найменьше число з масиву
+// let arrayMin = (array) => {
+//     let min = array[0];
+//     for (const arreyElement of array) {
+//         if(arreyElement < min){
+//             min = arreyElement;
+//         }
+//     }
+//     return min;
+// }
+// console.log(arrayMin(arrey));
+
+// - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
+// let arreyAdd = (array) => {
+//     let start = 0;
+//     for (const arrayElement of array) {
+//         start = arrayElement + start;
+//     }
+//     return start
+// }
+// console.log(arreyAdd(arrey));
+
 // -створити функцію, яка приймає масив з 2х об'єктів, та міняє їх місцями.
+// let change = (arr) => {
+//     arr.splice(0, 2, arr[1],arr[0])
+//     return arr
+// }
+//
+// console.log(arrey2);
+// console.log(change(arrey2));
+
+
---
+++ JavaScript/lesson6/work/main.js
@@ -0,0 +1,180 @@
+// let a = 'hello ';
+// let b = 'world ';
+// let c ='mother fucker'
+// let d = 'PikACHu'
+// console.log(a.concat(b,c)); // додавання
+//
+// console.log(c.toUpperCase()); // великі букви
+// console.log(c.toLowerCase()); // малі букви
+// console.log(d.toLowerCase()); // малі букви
+//
+// console.log(d.endsWith('P')); // містить в кінці
+// console.log(d.startsWith('P')); // містить на початку
+// console.log(d.includes('ACH')); // має в собі значення
+//
+// console.log(d.charAt(0)); // виведе по індексу
+// console.log(d.indexOf('A')); // покаже індекс букви
+//
+// console.log(d.indexOf('A')); // покаже індекс першого
+// console.log(d.lastIndexOf('u')); // покаже індекс останнього
+//
+// console.log(d.replace('PikACHu', 'єбаний покемон')); // заміняє
+// console.log(d.replaceAll('PikACHu', 'єбаний покемон')); // заміняє всі символи
+//
+// console.log(d.substring(0,3)); // Обрізає з і до
+//
+// console.log(d.split('k')); // розділяє по букві і робить масив
+//
+// let user = {id:1, name: 'dima', surname: 'beloshapkin', tel: 380672757648}
+// console.log(user)
+//
+// let info = '1-dima-beloshapkin-380672757648'
+// let information = info.split('-');
+// let userNew = {
+//     id: information[0],
+//     name: information[1].toUpperCase(),
+//     surname: information[2],
+//     tel: information[3]
+// }
+// console.log(userNew); // розподілення по символу знов спліт
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let arrey = [11, 22, 33];
+// console.log(Array.isArray(arrey)); // перевірка на масив
+//
+// console.log(arrey)
+// arrey.push(10, 20, 30) // додавання для масивів
+// console.log(arrey)
+//
+// arrey.unshift(101) // додавання на початок
+// console.log(arrey)
+//
+// arrey.pop() // забирає останнє
+// console.log(arrey)
+//
+// arrey.shift() // забирає перше
+// console.log(arrey)
+//
+// let join = arrey.join('-') // зєднання масиву в стрінгу символом
+// console.log(join)
+//
+// let item = ['one', 'two', 'thee', 'four'] // обєднання масивів
+// let concat = arrey.concat(item)
+// console.log(concat)
+//
+// let revers = arrey.reverse()
+// console.log(revers)
+//
+// let off = concat.indexOf('one'); // вказує індекс+1 вказаного елементу у масиві
+// console.log(off);
+//
+//
+// let arreyNew = [1, 2, 3, 4, 5, 6, 7, 8];
+// let splice = arreyNew.splice(0,2, 'через кому можна додати') // видалення з масиву
+// console.log(splice) // вирізаний шматок
+// console.log(arreyNew) // те що залишилось
+//
+// let arrey = [1, 2, 3, 4, 5];
+// let includes = arrey.includes(1)
+// console.log(includes)
+
+let users = [
+    {id: 1, name: 'Dima', age: 36, status: true},
+    {id: 2, name: 'Marianna', age: 37, status: true},
+    {id: 3, name: 'Vitaliy', age: 8, status: false},
+    {id: 4, name: 'Jura', age: 4, status: false},
+    {id: 5, name: 'Tetiana', age: 66, status: false},
+    {id: 6, name: 'Olya', age: 58, status: false},
+    {id: 7, name: 'Vasul', age: 32, status: true},
+    {id: 8, name: 'Khristina', age: 35, status: true},
+    {id: 9, name: 'Oleksandr', age: 5, status: false},
+    {id: 10, name: 'Anna', age: 2, status: false}
+];
+
+let matrix = [
+    [1, 2, 3],
+    [
+        4, 5, 6,
+        ['a', 'b', 'c']
+    ]
+];
+
+// let x = []
+// users.forEach(function (user) {
+//     if (user.status){
+//         x.push(user)
+//     }
+// console.log(user)
+// });
+//
+// console.log(x)
+//
+// users.forEach((value) => console.log(value)); // повертає новий масив з новими елементами
+
+// let filter = users.filter(value => value.status) // фільтр
+// let filter2 = users.filter(value => !value.status)
+// console.log(filter)
+// console.log(filter2)
+//
+// let filterNew1 = users.filter(function (user) {
+//     return user.status && user.age > 10
+// })
+// console.log(filterNew1)
+//
+// let filterNew2 = users.filter(user => !user.status && user.age > 10)
+// console.log(filterNew2)
+
+// users.map(function (user, index){
+//     user.idynaher = index+1
+//     return user
+// })
+//
+// console.log(users)
+
+// let sort = users.sort(function (a,b){ // сортування по цифрам
+//     return a.age - b.age
+// })
+// console.log(sort)
+//
+// let sortLength = users.sort((a,b) => a.name.length - b.name.length)
+// console.log(sortLength)
+
+// let find = users.find(user => user.status) // пошук на мінімалках
+// console.log(find)
+//
+// let every = users.every(user => typeof user.name==='string') // перевірка на відповідність умові
+// console.log(every)
+//
+// let some = users.some(user => user.name==='Dima') // перевірка на відповідність одній умові
+// console.log(some)
+
+// let flat = matrix.flat(2) //Обєднати в один масив в площину (2 це глибина)
+// console.log(flat);
+
+// let matrixNew = matrix.flatMap(value => value + 'краказябра')
+// console.log(matrixNew)
+
+// let reduce = arrey.reduce((previousValue, currentValue) => {
+//     console.log(previousValue, currentValue);
+//     return previousValue+ currentValue
+//     },0)
+// console.log(reduce)
+
+// let reduceNew = users.reduce((basket, user) => {
+//     basket[user.id] = user
+//     return basket
+// }, {})
+// console.log(reduceNew);
+
+// let newreduce = users.reduce((backet, user) => {
+//     if (user.status) {
+//         backet.statusTrue.push(user);
+//     } else {
+//         backet.statusFalce.push(user);
+//     }
+//     return backet;
+//
+// }, {statusTrue: [], statusFalce: []})
+//
+// console.log(newreduce);
